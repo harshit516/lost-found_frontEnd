228a26d245c58300c71311a48c78eb81856fe523
harshit shukla <shuklaharshit516@gmail.com>
2019-09-26 22:11:43 +0530

    added swipabe drawers

--- src/components/Login/Logo.jsx
+++ src/components/Login/Logo.jsx
@@ -9,12 +9,12 @@ const Logo = () => {
                 border: "#04dbad 0.3rem solid",
                 textAlign: "center",
                 fontSize: "4.6rem",
-                padding: "2.4rem",
+                padding: "2.25rem",
                 borderRadius: "10rem",
                 width: "14rem",
                 display: "flex",
                 alignItems: "center",
-                height: "13.5rem"
+                height: "13.7rem"
             }}
         >
             L&P
--- src/components/common/MenuButton.jsx
+++ src/components/common/MenuButton.jsx
@@ -2,9 +2,42 @@ import React, { useContext } from "react";
 import Button from "../common/Button";
 import MenuIcon from "@material-ui/icons/Menu";
 import MainContext from "../../context/main/mainContext";
-
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 const MenuButton = () => {
     const mainContext = useContext(MainContext);
+    const sideList = side => (
+        <div
+            style={{ width: "250px" }}
+            role="presentation"
+            onClick={mainContext.toggle_Drawer_Close}
+            onKeyDown={mainContext.toggle_Drawer_Close}
+        >
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <MailIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={"All mails"} />
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={"Trash"} />
+                </ListItem>
+            </List>
+        </div>
+    );
     return (
         <div>
             <Button
@@ -12,6 +45,13 @@ const MenuButton = () => {
                 color="primary"
                 icon={<MenuIcon fontSize="large" />}
             ></Button>
+            <SwipeableDrawer
+                open={mainContext.drawer_state}
+                onClose={mainContext.toggleDraweClose}
+                onOpen={mainContext.toggleDraweOpen}
+            >
+                {sideList("left")}
+            </SwipeableDrawer>
         </div>
     );
 };
